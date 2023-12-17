b84842bc787bc931f02dbc3a42eb7649ad2c693b
Afish718 <18851177630@163.com>
2023-12-17 22:18:29 +0800

    add file

---
+++ command_test_obs.sh
@@ -0,0 +1,3 @@
+# CUDA_VISIBLE_DEVICES=1 python test_obs.py --dump_dir logs/dump_full_model_obs  --checkpoint_path logs/log_full_model/checkpoint.tar --seg_checkpoint_path logs/log_insseg/checkpoint.tar --collision_thresh 0 --camera realsense --dataset_root /data/mahaoxiang/graspnet
+
+CUDA_VISIBLE_DEVICES=0 python test_obs_our.py --dump_dir logs/dump_full_model  --checkpoint_path /home/hjk/workspace/grasp/Scale-Balanced-Grasp/Checkpoints/log_full_model/checkpoint.tar --seg_checkpoint_path /home/hjk/workspace/grasp/Scale-Balanced-Grasp/Checkpoints/log_insseg/checkpoint.tar  --collision_thresh 0 --camera realsense --dataset_root /home/hjk/workspace/grasp/graspnet_dataset/test_data
